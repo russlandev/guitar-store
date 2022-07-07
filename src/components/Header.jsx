bf53b179db0d1257eb514f2b47da844d895cd141
RuslanTkachenko <ruslan.tkachenko.99@gmail.com>
2022-07-08 02:32:47 +0300

    added slider and item card

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,37 +3,39 @@ import styles from "./Header.module.scss";
 
 const Header = () => {
     return (
-        <div className="fixed top-0 w-full">
-            <div className={styles.header}>
-                <div className={styles.wrapper}>
-                    <a className={styles.logo} href="#">
-                        LOGO
-                    </a>
-                    <ul>
-                        <li className={styles.header__item}>
-                            <a href="#">GUITARS</a>
-                        </li>
-                        <li className={styles.header__item}>
-                            <a href="#">BASSES</a>
-                        </li>
-                        <li className={styles.header__item}>
-                            <a href="#">ACCESORIES</a>
-                        </li>
-                        <li className={styles.header__item}>
-                            <a href="#">SIGN IN</a>
-                        </li>
-                        <li className={styles.header__item}>
-                            <a href="#">CART</a>
-                        </li>
-                    </ul>
-                    <ul className="right-0 absolute mr-8">
-                        <li className={styles.header__item}>
-                            <a href="#">SIGN IN</a>
-                        </li>
-                        <li className={styles.header__item}>
-                            <a href="#">CART</a>
-                        </li>
-                    </ul>
+        <div className="mb-20">
+            <div className="fixed top-0 w-full">
+                <div className={styles.header}>
+                    <div className={styles.wrapper}>
+                        <a className={styles.logo} href="#">
+                            LOGO
+                        </a>
+                        <ul>
+                            <li className={styles.header__item}>
+                                <a href="#">GUITARS</a>
+                            </li>
+                            <li className={styles.header__item}>
+                                <a href="#">BASSES</a>
+                            </li>
+                            <li className={styles.header__item}>
+                                <a href="#">ACCESORIES</a>
+                            </li>
+                            <li className={styles.header__item}>
+                                <a href="#">SIGN IN</a>
+                            </li>
+                            <li className={styles.header__item}>
+                                <a href="#">CART</a>
+                            </li>
+                        </ul>
+                        <ul className="right-0 absolute mr-8">
+                            <li className={styles.header__item}>
+                                <a href="#">SIGN IN</a>
+                            </li>
+                            <li className={styles.header__item}>
+                                <a href="#">CART</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/ItemPage.jsx
+++ src/components/ItemPage.jsx
@@ -1,49 +1,17 @@
 import React from "react";
-
-const guitar = {
-    id: "BODEN PROG NX 6 EARTH GREEN",
-    name: "BODEN PROG NX 6 EARTH GREEN",
-    img: [
-        "https://strandbergguitars.com/cdn-cgi/image/onerror=redirect,quality=85,format=auto,metadata=copyright,width=1066,height=1600,fit=cover/wp-content/uploads/2021/09/bd6tct-21p-l-q-gn_6.png",
-        "https://strandbergguitars.com/cdn-cgi/image/onerror=redirect,quality=85,format=auto,metadata=copyright,width=768/wp-content/uploads/2021/09/bd6tct-21p-l-q-gn_2.png",
-        "https://strandbergguitars.com/cdn-cgi/image/onerror=redirect,quality=85,format=auto,metadata=copyright,width=768/wp-content/uploads/2021/09/bd6tct-21p-l-q-gn_b.png",
-        "https://strandbergguitars.com/cdn-cgi/image/onerror=redirect,quality=85,format=auto,metadata=copyright,width=768/wp-content/uploads/2021/09/bd6tct-21p-l-q-gn_c.png",
-        "https://strandbergguitars.com/cdn-cgi/image/onerror=redirect,quality=85,format=auto,metadata=copyright,width=1920/wp-content/uploads/2021/09/nx-prog-6-family.png",
-    ],
-    descr: "Aggressive and sophisticated, the Prog NX models are an extension of the Original model design with the new improved EGS Pro Rev7 Tremolo System and the smooth and slick-playing Richlite fretboard. With a more cutting tone than the Original, the Prog models are ideal for modern progressive and metal players using high-gain amplification systems. The versatile Suhr™ pickups and switching system provide a wide range of sounds for virtually any playing style and musical genre.",
-    specs: {
-        general: [
-            "Bolt-On Construction with new sculpted neck joint heel",
-            "Chambered Swamp Ash Body with Arm and Torso Carves",
-            "Solid Maple Top with 4A Flame Maple or Quilt Maple veneer",
-            "  Available Finishes: Charcoal Black for Flame, Earth Green for Quilt",
-            "Approximate Weight: 2.3kg / 5 lbs +/- 10%",
-            "Manufactured in Indonesia",
-        ],
-        neck: [
-            "Maple neck – Quartersawn, Carbon Fiber reinforced",
-            "EndurNeck™ Profile",
-            "Richlite Fretboard",
-            "20” Fretboard Radius",
-            "24 Frets",
-            "Jescar 51100 Stainless Steel Fretwire (57110 for zero fret)",
-            "Standard tuning EADGBE",
-        ],
-        profile: [
-            "H-H Pickup Configuration",
-            "Neck: Suhr SSV Neck",
-            "Bridge: Suhr SSH+ Bridge",
-            "5-Way Pickup Selector",
-            "Master Volume + Master Tone",
-        ],
-    },
-};
+import Slider from "./Slider";
+import { guitars } from "../data/mockedData";
+import styles from "./ItemPage.module.scss";
 
 const ItemPage = () => {
+    const guitar = guitars[4];
     return (
-        <div className="container">
-            <div className="">
-
+        <div className={styles.itempage__wrapper}>
+            <div className={styles.itempage__slider}>
+                <Slider guitar={guitar} sliderWidth={450} />
+            </div>
+            <div className={styles.itempage__details}>
+                <h2 className={styles.item__title}>{guitar.name}</h2>
             </div>
         </div>
     );
